621f66704648781d9692b3425a39feaced3b0480
rangerx02 <gean.9720@gmail.com>
2025-03-05 16:18:49 -0500

    Actualizaci├│n autom├ítica de Proyectos Subidos

---
+++ Actualizacion automaica de Carpetas Proyectos Eventos.bat
@@ -0,0 +1,7 @@
+@echo off
+cd "C:\Users\Gfmt\Desktop\Todo\Univalle Gean\6-Univalle Sexto Semestre\Eventos\Github\ProyectosEventos"
+git pull origin HEAD
+git add .
+git commit -m "Actualización automática de Proyectos Subidos"
+git push origin HEAD
+echo Actualización completada
