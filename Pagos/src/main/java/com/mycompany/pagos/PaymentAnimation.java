f794004347badfc059b3462c102d6a152641ba1d
rangerx02 <gean.9720@gmail.com>
2025-03-07 12:27:50 -0500

    Actualizaci├│n autom├ítica de la carpeta Pagos

--- Pagos/src/main/java/com/mycompany/pagos/PaymentAnimation.java
+++ Pagos/src/main/java/com/mycompany/pagos/PaymentAnimation.java
@@ -6,13 +6,42 @@ package com.mycompany.pagos;
 
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
-
+import java.net.URL;
 /**
  *
  * @author Gfmt
  */
 // Clase de animaciones de pago
+class PaymentAnimation {
+    public static void showAnimation(Payment paymentMethod) {
+        String imagePath = "";
 
+        if (paymentMethod instanceof CashPayment) {
+            imagePath = "/images/cash.png";
+        } else if (paymentMethod instanceof CreditCardPayment) {
+            imagePath = "/images/credit_card.png";
+        } else if (paymentMethod instanceof DebitCardPayment) {
+            imagePath = "/images/debit_card.png";
+        } else if (paymentMethod instanceof MoneyCheckPayment) {
+            imagePath = "/images/check.png";
+        } else if (paymentMethod instanceof PayPalPayment){
+            imagePath = "/images/paypal.png";
+        }
+                
+
+        // Cargar la imagen desde el classpath
+        URL imageUrl = PaymentAnimation.class.getResource(imagePath);
+
+        if (imageUrl == null) {
+            JOptionPane.showMessageDialog(null, "Error: No se pudo cargar la imagen: " + imagePath);
+            return;
+        }
+
+        ImageIcon icon = new ImageIcon(imageUrl);
+        JOptionPane.showMessageDialog(null, "Pago procesado correctamente.", "Método de Pago", JOptionPane.INFORMATION_MESSAGE, icon);
+    }
+}
+/*
 class PaymentAnimation {
     public static void showAnimation(Payment paymentMethod) {
         String message = "";
@@ -34,6 +63,8 @@ class PaymentAnimation {
             message = "Procesando pago con PayPal...";
             imagePath = "images/paypal.png";
         }
+        // Cargar la imagen correctamente desde la carpeta de recursos
+        ImageIcon icon = new ImageIcon(PaymentAnimation.class.getResource(imagePath));
 
         // Cargar la imagen
         ImageIcon icon = new ImageIcon(imagePath);
@@ -43,7 +74,7 @@ class PaymentAnimation {
     }
 }
 
-
+*/
 
 
 
